fcdc7e8cdfbfed17f5d4f39cacd67db9b68ece39
adashuai5 <ad873606339@gmail.com>
2018-10-03 00:45:16 +0800

    完成 button-group 创建

---
+++ src/button-group.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+    .g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        > .g-button {
+            margin-left: -1px;
+            &:hover{
+                position: relative;
+                z-index: 1;
+            }
+            border-radius: 0;
+            &:first-child {
+                border-top-left-radius: var(--button-radius);
+                border-bottom-left-radius: var(--button-radius);
+            }
+            &:last-child {
+                border-top-right-radius: var(--button-radius);
+                border-bottom-right-radius: var(--button-radius);
+            }
+        }
+    }
+</style>
