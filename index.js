5388733f7ee59ab70fb5c4195f167566b1caf9a8
adashuai5 <ad873606339@gmail.com>
2018-10-05 01:20:04 +0800

    使用 npm link 代替每次改动发布到 npmjs

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from './src/icon'
 
-export {Button, ButtonGroup, Icon}
+export {Button, ButtonGroup, Icon} 
--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}"
+    <button class="g-button ada1" :class="{[`icon-${iconPosition}`]: true}"
      @click="$emit('click')">
         <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
         <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
