fc943fc72f82c5319249d64814c1c400949580b6
adashuai5 <ad873606339@gmail.com>
2018-10-06 21:39:06 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="span && [`col-${span}`]">
-        <slot></slot>
+    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+         :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+        <div style="border: 1px solid red; height: 100px;">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
@@ -9,20 +12,32 @@
         props: {
             span: {
                 type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            }
+        },
+        data() {
+            return {
+                gutter: 0
             }
         }
     }
 </script>
 <style lang="scss" scoped>
     .col {
-        height: 100px;
         width: 50%;
-        background: #aaa;
-        border: 1px solid red;
+        padding: 0 20px;
         $class-prefix: col-;
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n / 24) * 100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) * 100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,23 @@
-<template>
+<template :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
     <div class="row">
         <slot></slot>
     </div>
 </template>
+<script>
+    export default {
+        name: 'WheelsRow',
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        mounted() {
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
+            })
+        }
+    }
+</script>
 <style lang="scss" scoped>
     .row {
         display: flex;
